57dd38e1d07f27ff058494976a6fbed1a7629859
Sanchit Kalra <sanchitkalra@Sanchits-MacBook-Air.local>
2020-10-06 22:51:03 +0530

    Added even  more from Arrays and Lists

--- wip/assignment_sort_0_1.py
+++ wip/assignment_sort_0_1.py
@@ -1,9 +1,21 @@
 arr = [0, 1, 1, 0, 1, 0, 1]
+arr = [1, 0, 1, 1, 0, 1, 0, 1]
+#arr = [0, 1, 0, 1, 0]
 
 for k in range(len(arr)):
     if arr[k] == 0:
-        arr[k], arr[0] = arr[0], arr[k]
+        arr.pop(k)
+        arr.insert(0, 0)
     else:
-        arr[k], arr[-1] = arr[-1], arr[k]
+        arr.pop(k)
+        arr.append(1)
+
+for k in range(len(arr)):
+    if arr[k] == 0:
+        arr.pop(k)
+        arr.insert(0, 0)
+    else:
+        arr.pop(k)
+        arr.append(1)
 
 print(arr)
